05331f90c55b8b9d65e9494aeae16f43745ca46e
Xavier Basty <xavier@hexbee.net>
2022-07-13 17:00:02 +0200

    Add scaffolding for web-app

---
+++ web/app/src/main.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import 'primereact/resources/themes/mdc-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+import {Metric} from "web-vitals/src/types";
+import reportWebVitals from './reportWebVitals';
+
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
+import App from './App'
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
